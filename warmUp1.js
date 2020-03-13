100485bae0035bdf637a4e03353bf76b64d92c99
oussema-sferi <oussema.sferi@gmail.com>
2020-03-13 09:49:00 +0100

    questions solved

--- warmUp1.js
+++ warmUp1.js
@@ -4,3 +4,58 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+
+//1-
+
+function combine() {
+
+	var firstName = 'oussema':
+
+	var lastName = 'sferi';
+
+return firstName + ' ' + lastName;
+
+}
+
+//2- 
+
+function multipleOf13() {
+
+	if ((13 % 3) === 0) {
+
+	return '13 is a multiple of 3';
+	}
+
+return '13 is not a multiple of 3';
+
+}
+
+
+//3-
+
+function average() {
+
+var age = [13, 14, 13, 15, 16, 17, 19, 13, 16, 15];
+
+	var sum = 0; 
+
+	for(var i = 0; i < age.length; i = i +1) {
+
+	sum = sum + age[i];
+
+	}
+
+	return sum / age.length;
+
+}
+
+//4-
+
+function age() {
+
+	var myAge = 31;
+	
+	return myAge * 365 * 24 * 60 * 60;
+
+}
