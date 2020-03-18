77110bca597fd56572a81666148033da4754413c
oussema-sferi <oussema.sferi@gmail.com>
2020-03-18 10:38:15 +0100

    not all exercises completed, will complete them after 5 pm

--- warmUp5.js
+++ warmUp5.js
@@ -3,23 +3,79 @@
 // - returns whichever number is the greater (higher) number.
 // ex greaterNum(5, 10) => "The greater number of 5 and 10 is 10."
 
+function greaterNum(a, b) {
+
+var result;
+
+	if(a > b) {
+
+		result = "The greater number of " + a + " and " + b + " is " + a + ".";
+
+	} else if (a < b) {
+
+		result = "The greater number of " + a + " and " + b + " is " + b + ".";
+
+	}
+
+	return result;
+}
+
 // 2) Write a function named isEven using  for loop that
 // -  iterate from x to y.
 // -  return array contain the even values,
 //  ex:  isEven(1,10) => [2,4,6,8,10]
 
 // function isEven(x, y) {
-
+//     var arr = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         while (x <= y) {
+//             if (x % 2 === 0) {
+//                 arr[i] = arr.push(x)
+//             }
+//             x++
+//         }
+//     }
+//     return arr;
 // }
+
+
+
 //3) write a function named sum that
 // - Use a while loop to add up the numbers from x to y.
 // ex sum(1,5) => 15
+
+function sum(a, b) {
+
+var s = 0;
+
+	while(a <= b) {
+
+		s = s + a;
+
+		a = a + 1;
+	}
+
+	return s;
+}
 
 //4) Write a function named factorial that
 // -  Use Recursion to calculate the factorial of a number
 // -  the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n
 // - 5! = 5*4*3*2*1 = 120
 // ex : factorial(5) => 120
+
+function factorial(n) {
+
+var f;
+
+	if (n === 0 || n === 1) {
+
+		return 1;
+	}
+
+	return f = n * factorial(n - 1);
+
+}
 
 //5) write a function named decimals
 //- the function will format a number up to specified decimal places
@@ -32,3 +88,47 @@
 //      decimals(2.100212, 3) ==> '2.100'
 //      decimals(2100, 2) ==> '2100.00'
 //
+
+function decimals(a, b) {
+
+	var r;
+
+	var dec;
+
+	var numToStr;
+
+	if(isNaN(a) || isNaN(b)) {
+
+		return 'false';
+
+	}
+
+		r = a % 1;
+
+		if(r === 0) {
+
+			dec = a + '.'
+
+				for(var i = 0; i < b; i = i + 1) {
+
+				dec = dec + '0';
+
+			}
+
+		} else {
+
+			numToStr = a.toString();
+
+				for(var i = 0; i < b; i = i + 1) {
+
+				dec = dec + '0';
+
+			}
+
+
+
+		}
+														//not completeeeeed
+
+	return dec;
+}
