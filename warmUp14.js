588afd4938bb38dd460020686a724317950e4a06
oussema-sferi <oussema.sferi@gmail.com>
2020-03-28 10:09:09 +0100

    o

--- warmUp14.js
+++ warmUp14.js
@@ -7,3 +7,37 @@
 // array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 // array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 // array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+function isSubset(arr1, arr2) {
+
+	for(var i = 0; i < arr1.length; i += 1) {
+
+			if(arr1[i] === arr2[0]) {
+
+				var counter = 1;
+
+				for(var j = 1; j < arr2.length; j += 1) {
+
+					var k = i;
+
+					if( arr[k+1]===  arr2[j]) {
+
+					counter += 1;
+
+					k += 1;
+
+					
+					}
+
+				}	
+
+					if(counter === arr2.length) {
+
+						return true;
+
+					}
+
+				return false;
+			}
+	}	
+}
